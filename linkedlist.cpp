7f90569295358e4aecf653f763b60efcbd1fa1b7
Kris Kahn <krkahn@gmail.com>
2023-04-05 15:26:00 -0400

    linked_list modularization

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,46 +1,54 @@
 #include <iostream>
 
-struct Node{
-  int data;
-  Node* next;
+struct ListNode{
+  int val;
+  ListNode* next;
 };
 
+void insertNode(ListNode *&head, ListNode *&tail, int val) {
+    ListNode *newNode = new ListNode;
+    newNode->val = val;
+    newNode->next = nullptr;
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode; 
+    tail = newNode;
+}
+
 // Implements recursion
-void printLinkedList(Node* head) {
+void printLinkedList(ListNode* head) {
   if (head == NULL){
     return;
   }
-  std::cout << head->data << std::endl;
+  std::cout << head->val << std::endl;
   printLinkedList(head->next);
 }
 
 // Implements recursion
-void printLinkedListInReverse(Node* head) {
+void printLinkedListInReverse(ListNode* head) {
   if (head == NULL){
     return;
   }
   printLinkedListInReverse(head->next);
-  std::cout << head->data << std::endl;
+  std::cout << head->val << std::endl;
 }
 
 int main(){
   // Create first node
-  Node* head = new Node;
-  head->data = 1;
-  head->next = NULL;
+    ListNode *list1 = nullptr;
+    ListNode *tail = nullptr;
+    insertNode(list1, tail, 6);
+    insertNode(list1, tail, 9);
+    insertNode(list1, tail, 4);
+    insertNode(list1, tail, 2);
+    // print the linked list
+    ListNode *curr = list1;
 
-  // Add more nodes
-  Node* second = new Node;
-  second->data = 69;
-  second->next = NULL;
-  head->next = second;
-  
-  Node* third = new Node;
-  third->data = 420;
-  third->next = NULL;
-  second->next = third;
-
-  printLinkedList(head);
-  printLinkedListInReverse(head);
+  printLinkedList(list1);
+  std::cout << "\nreversed:\n" << std::endl;
+  printLinkedListInReverse(list1);
  return 0;
 }
